fcdbb865a214ad57c9325d0799275788a765aad7
Stanisds12 <53280516+Stanisds12@users.noreply.github.com>
2021-03-25 09:37:51 -0400

    Update todo_list.sql

--- server/todo_list.sql
+++ server/todo_list.sql
@@ -2,6 +2,6 @@ CREATE DATABASE todo_list;
 CREATE TABLE todo
 (
   todo_id serial NOT NULL,
-  descripcion character varying(255),
+  descripcion varchar(255),
   CONSTRAINT todo_pkey PRIMARY KEY (todo_id)
 )
